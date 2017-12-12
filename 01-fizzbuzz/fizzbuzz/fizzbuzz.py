d037badf4db3632075b0d7579fc6b4c0375fb559
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-12-12 17:11:35 +0100

    [GREEN] must return FizzBuzz if the number is divisible by 3 and 5.

--- 01-fizzbuzz/fizzbuzz/fizzbuzz.py
+++ 01-fizzbuzz/fizzbuzz/fizzbuzz.py
@@ -1,5 +1,7 @@
 def calculate(value):
-    if value % 3 == 0:
+    if value % 3 == 0 and value % 5 == 0:
+        return 'FizzBuzz'
+    elif value % 3 == 0:
         return 'Fizz'
     elif value % 5 == 0:
         return 'Buzz'
