3d895d6c8213fd4f6bc40e7a9c89b986db757fbb
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-12-12 16:45:07 +0100

    [RED] must return the value when the value is not 3,5 or divisible by 3 or 5.

--- 01-fizzbuzz/tests/test_fizzbuzz.py
+++ 01-fizzbuzz/tests/test_fizzbuzz.py
@@ -9,3 +9,6 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_return_buzz_when_value_is_5(self):
         self.assertEqual(fizzbuzz.calculate(5), "Buzz")
 
+    def test_should_return_the_value_when_value_is_not_3_or_5_or_divisors(self):
+        self.assertEqual(fizzbuzz.calculate(1), "1")
+
