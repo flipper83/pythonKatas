e249fa9083aeb08b2828520ab495ebe799dc37ea
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-12-12 16:38:46 +0100

    [RED] First test, check return fizz when the value is 3.

---
+++ 01-fizzbuzz/tests/test_fizzbuzz.py
@@ -0,0 +1,7 @@
+import unittest
+from fizzbuzz import fizzbuzz
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_should_return_fizz_when_value_is_3(self):
+        self.assertEqual(fizzbuzz.calculate(3), "fizz")
