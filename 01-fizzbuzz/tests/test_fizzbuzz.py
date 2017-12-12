62040fe1f3e3404a53b029a270d19c1b1713f221
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-12-12 16:43:47 +0100

    [Refactor] rename test type in one method.

--- 01-fizzbuzz/tests/test_fizzbuzz.py
+++ 01-fizzbuzz/tests/test_fizzbuzz.py
@@ -6,5 +6,6 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_return_fizz_when_value_is_3(self):
         self.assertEqual(fizzbuzz.calculate(3), "Fizz")
 
-    def test_sould_return_buzz_when_value_is_5(self):
+    def test_should_return_buzz_when_value_is_5(self):
         self.assertEqual(fizzbuzz.calculate(5), "Buzz")
+
