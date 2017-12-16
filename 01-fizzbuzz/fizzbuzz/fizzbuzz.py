50a01475d14efebd05e32b149a91c7bda58e1b35
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-12-16 14:46:26 +0100

    Add pylint configuration for name tests and disable unecessary docstring.

--- 01-fizzbuzz/fizzbuzz/fizzbuzz.py
+++ 01-fizzbuzz/fizzbuzz/fizzbuzz.py
@@ -1,5 +1,6 @@
 def calculate(value):
-    if (is_divisible(value, 3) and is_divisible(value, 5)) or (contains(value, '3') and contains(value, '5')):
+    if (is_divisible(value, 3) and is_divisible(value, 5)) \
+            or (contains(value, '3') and contains(value, '5')):
         return 'FizzBuzz'
     elif is_divisible(value, 3) or contains(value, '3'):
         return 'Fizz'
